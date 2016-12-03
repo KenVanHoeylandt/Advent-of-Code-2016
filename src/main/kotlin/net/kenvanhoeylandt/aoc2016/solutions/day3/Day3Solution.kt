f47c59ac0156e8bd9743b28fdc4aa54c74d558ac
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-12-03 21:48:24 +0100

    day 3 solution part 2

--- src/main/kotlin/net/kenvanhoeylandt/aoc2016/solutions/day3/Day3Solution.kt
+++ src/main/kotlin/net/kenvanhoeylandt/aoc2016/solutions/day3/Day3Solution.kt
@@ -18,6 +18,33 @@ class Day3Solution : Solution(3) {
 
     @Throws(Exception::class)
     override fun solvePartTwo(input: String): String {
-        return "nothing"
+        // Get the TriangleSides objects as with part one
+        val observableTriangleSidesFromPartOne = Observable.just(input)
+                .flatMap { Observable.fromIterable(it.split("\n")) } // separate lines into separate Strings
+                .map(::triangleSidesOf)
+
+        // Convert TriangleSides instances to column data (list of integers that represent the column)
+        val columnOne = observableTriangleSidesFromPartOne.map{ it.a }
+        val columnTwo = observableTriangleSidesFromPartOne.map{ it.b }
+        val columnThree = observableTriangleSidesFromPartOne.map{ it.c }
+
+        // Get a single list from the 3 columns
+        val numberList = Observable.merge(columnOne, columnTwo, columnThree)
+                .toList()
+                .blockingGet()
+
+        // Create TriangleSides again from the column input data
+        val maxIndex = (numberList.size/3) - 1
+        val triangleSidesList = (0..maxIndex).map {
+            val index = it * 3
+            TriangleSides(numberList[index], numberList[index+1], numberList[index+2])
+        }
+
+        // Count valid TriangleSides instances
+        return Observable.fromIterable(triangleSidesList)
+                .filter(TriangleSides::isValid)
+                .count()
+                .blockingGet()
+                .toString()
     }
 }
