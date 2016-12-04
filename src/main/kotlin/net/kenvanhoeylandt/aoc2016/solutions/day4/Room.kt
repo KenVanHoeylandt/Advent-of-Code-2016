2af5287d0f654e08d1aa15675f130136eb6485c1
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-12-05 00:01:21 +0100

    added javadoc for day 4

--- src/main/kotlin/net/kenvanhoeylandt/aoc2016/solutions/day4/Room.kt
+++ src/main/kotlin/net/kenvanhoeylandt/aoc2016/solutions/day4/Room.kt
@@ -3,22 +3,33 @@ package net.kenvanhoeylandt.aoc2016.solutions.day4
 import java.util.regex.Pattern
 import kotlin.comparisons.compareBy
 
+/**
+ * A room as described in the assignment.
+ */
 data class Room(val name: String, val sectorId: Int, val checksum: String) {
+
+    /**
+     * Validity check for Part 1
+     */
     fun isValid(): Boolean {
+        // We're not interested in dashes
         val cleanedName = name.replace("-", "")
+        // Find out how many times a character is used
         val characterMap = mutableMapOf<Char, Int>()
         for (char in cleanedName) {
             val count = characterMap.getOrElse(char) { 0 } + 1
             characterMap.put(char, count)
         }
 
+        // Sort the character map entries: first by amount of times seen (value, from large to small),
+        // then by character (alphabetically, from small to large)
         val nameChecksumParts = characterMap.entries
                 .sortedWith(compareBy({ -it.value }, { it.key }))
                 .map { it.key }
                 .subList(0, 5)
 
+        // Gather all characters into a String
         val nameChecksum = StringBuilder()
-
         nameChecksumParts.forEach {
             nameChecksum.append(it)
         }
@@ -26,13 +37,20 @@ data class Room(val name: String, val sectorId: Int, val checksum: String) {
         return nameChecksum.toString() == checksum
     }
 
+    /**
+     * Shift cipher decryption for Part 2
+     */
     fun decrypt(): String {
         val cleanedName = name.replace("-", " ")
         val decrypted = StringBuilder()
+
+        // go through each character
         for (character in cleanedName) {
             if (character != ' ') {
+                // ignore whole alphabet cycles
                 val remainder = sectorId % 26
                 val newCharacter = character + remainder
+                // ensure we have a valid character still
                 if (newCharacter > 'z') {
                     decrypted.append(newCharacter - 26)
                 } else {
@@ -42,10 +60,14 @@ data class Room(val name: String, val sectorId: Int, val checksum: String) {
                 decrypted.append(character)
             }
         }
+
         return decrypted.toString()
     }
 }
 
+/**
+ * Convert input text into a Room instance
+ */
 fun roomOf(text: String): Room {
     val pattern = Pattern.compile("([a-z0-9-]+)-([0-9]+)\\[([a-z]+)\\]")
     val matcher = pattern.matcher(text)
